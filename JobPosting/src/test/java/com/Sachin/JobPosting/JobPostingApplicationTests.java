a8810017abf53f4aec6daf39fb7503d1a1652b64
Sachin Uthpala <gunasekara.s.u.d.a@gmail.com>
2024-04-21 23:59:45 +0530

    first commit

---
+++ JobPosting/src/test/java/com/Sachin/JobPosting/JobPostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sachin.JobPosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
